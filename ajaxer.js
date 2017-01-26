8264308909018198ea44949c03bf728d3d3fc790
a_proskurin <a.proskurin@s-e.su>
2017-01-26 12:03:25 +0300

    ajax examples

--- ajaxer.js
+++ ajaxer.js
@@ -71,21 +71,3 @@ var ajaxer = (function () {
 	
 	return this;
 })();
-
-ajaxer.elements = [
-'link[rel="canonical"]',
-'title',
-'meta[name="description"]',
-'meta[name="keywords"]',
-'meta[property="og:type"]',
-'meta[name="og:title"]',
-'meta[name="og:description"]',
-'meta[name="og:site_name"]',
-'meta[name="og:url"]',
-'meta[name="og:image"]',
-'h1',
-'.play-list'
-];
-
-ajaxer.get('http://mp3musicbit.com/download/%25D1%2583%25D1%2580%25D0%25B0%25D0%25BB%25D1%258C%25D1%2581%25D0%25BA%25D0%25B8%25D0%25B5%2520%25D0%25BF%25D0%25B5%25D0%25BB%25D1%258C%25D0%25BC%25D0%25B5%25D0%25BD%25D0%25B8%2520%25D0%25BC%25D1%2583%25D0%25B7%25D1%258B%25D0%25BA%25D0%25B0/', function () {console.log('fail');});
-ajaxer.get('http://mp3musicbit.com/download/Michael%2520Malarkey%2520-%2520Scars/', function () {console.log('fail');});
